b64c5882c5548719f2a32818a5b5065aac86b487
RikThePixel <Rikdenbreejen@hotmail.nl>
2021-10-22 09:48:23 +0200

    [Change] Add toolbar

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,32 +1,39 @@
-﻿import React, { } from 'react';
+﻿import React from "react";
 import { PinNote, PinBoard } from "../components/BoardElements";
+import Popup from "reactjs-popup";
 
-import "./Board.scss"
+import "reactjs-popup/dist/index.css";
+import "./Board.scss";
 
 export default function Board(props) {
-    document.title = "Pinnote - Board";
+  document.title = "Pinnote - Board";
 
-    const BoardNotes = [
-        {
-            title: "aa",
-            text: "Some text",
-            position: {
-                x: 200,
-                y: 300
-            }
-        },
-        {
-            title: "bb",
-            text: "MORE TEXT"
-        }
-    ];
-    for (var i = 0; i < BoardNotes.length; i++) {
-        BoardNotes[i] = (<PinNote key={i} data={BoardNotes[i]} />);
-    }
+  const BoardNotes = [
+    {
+      title: "aa",
+      text: "Some text",
+      position: {
+        x: 200,
+        y: 300,
+      },
+    },
+    {
+      title: "bb",
+      text: "MORE TEXT",
+    },
+  ];
+  for (var i = 0; i < BoardNotes.length; i++) {
+    BoardNotes[i] = <PinNote key={i} data={BoardNotes[i]} />;
+  }
 
-    return (
-        <PinBoard>
-            {BoardNotes}
-        </PinBoard>
-    );
+  return (
+    <div>
+      <div className="BoardsPage-Header" >
+        <button>
+            aaa
+        </button>
+      </div>
+      <PinBoard>{BoardNotes}</PinBoard>
+    </div>
+  );
 }
