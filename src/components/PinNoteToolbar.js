594ddcfcfccaad9984262ef261644480e4c2b871
RikThePixel <Rikdenbreejen@hotmail.nl>
2021-10-22 10:17:16 +0200

    [Addition] Toolbar.js

---
+++ src/components/PinNoteToolbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function PinNoteToolbar(props) {
+    return (
+        <div className="PinNote-Toolbar">
+            {props.children}
+        </div>
+    )
+}
