147cfe7a470b0aefaf9dd0d9a8180bfcf251596c
RikThePixel <Rikdenbreejen@hotmail.nl>
2021-11-05 00:49:55 +0100

    [Addition] User can update board title

--- src/components/BoardElements.js
+++ src/components/BoardElements.js
@@ -1,5 +1,6 @@
-﻿import "./BoardElements.scss";
+﻿import "./BoardElements/BoardElements.scss";
 import PinNote from "./BoardElements/PinNote";
 export {PinBoard} from "./BoardElements/PinBoard";
 export {PinBoardItem} from "./BoardElements/PinBoardItem";
+export {PinNoteToolbar} from "./BoardElements/PinNoteToolbar";
 export default PinNote;
--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,41 +1,67 @@
-import React, {useRef} from "react";
-import { PinBoard } from "../components/BoardElements";
-import { PinNoteToolbar } from '../components/PinNoteToolbar';
-import { Button } from 'react-bootstrap';
-import Popup from "reactjs-popup";
+import React, { createRef, Component } from "react";
+import { PinBoard, PinNoteToolbar } from "../components/BoardElements";
+import MakeWriteable from "../components/MakeWriteable";
+import { Button } from "react-bootstrap";
 import "reactjs-popup/dist/index.css";
 import "./Board.scss";
 
-export default function Board(props) {
-  document.title = "Pinnote - Board";
+export default class Board extends Component {
+  constructor(props) {
+    super(props)
+    document.title = "Pinnote - Board";
 
-  const BoardNotes = [
-    {
-      noteId: Math.random(1, 99999),
-      title: "aa",
-      text: "Some text",
-      position: {
-        x: 200,
-        y: 300,
-      },
-    },
-    {
-      noteId: Math.random(1, 99999),
-      title: "bb",
-      text: "MORE TEXT",
-    },
-  ];
-  
-  const newNoteButton = useRef()
+    this.state = {
+      title: "My board",
+      notes: [
+        {
+          noteId: Math.random(1, 99999),
+          title: "aa",
+          text: "Some text",
+          position: {
+            x: 200,
+            y: 300,
+          },
+        },
+        {
+          noteId: Math.random(1, 99999),
+          title: "bb",
+          text: "MORE TEXT",
+        },
+      ],
+    }
 
-  return (
-    <div className="page-container">
+    this.toolbarTitleRef = createRef();
+    this.newNoteButton = createRef();
+  }
+
+  updateTitle = function(div) {
+    this.setState({
+      ...this.state,
+      title: div.textContent
+    })
+  }.bind(this)
+
+  render() {
+    return (
+      <div className="page-container">
         <PinNoteToolbar>
-            <Button ref={newNoteButton}>
-                + Note
-            </Button>
+          <div className="me-auto">
+            <div className="pinBoard-Toolbar-Title" ref={this.toolbarTitleRef}>
+              <MakeWriteable
+                parentRef={this.toolbarTitleRef}
+                editStyle={{
+                  backgroundColor: "#FFF",
+                }}
+                onUnWriteable={this.updateTitle}
+              />
+              {this.state.title}
+            </div>
+          </div>
+          <Button ref={this.newNoteButton}>+ Note</Button>
         </PinNoteToolbar>
-        <PinBoard Notes={BoardNotes} newNoteButton={newNoteButton} />
-    </div>
-  );
+        <PinBoard Notes={this.state.notes} newNoteButton={this.newNoteButton} />
+      </div>
+    );
+  }
+  
 }
