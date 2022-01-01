e7f65b466de3db79c9482600e52e9923ff9a55d0
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-01-01 16:42:08 +0100

    Change attempt to fix

---
+++ src/App.js
@@ -0,0 +1,16 @@
+"use strict";
+import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
+
+import MainRoute from './routes/MainRoute';
+import store from './store';
+
+const App = () => {
+
+    return (
+        <ReduxProvider store={store}>
+            <MainRoute />
+        </ReduxProvider>
+    )
+}
+export default App;
