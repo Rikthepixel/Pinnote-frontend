f6a81cbac5396e7ac75f52ce1593aeb113da2d23
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-01-13 11:19:49 +0100

    Redirect to HTTPS

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import HttpsRedirect from "react-https-redirect";
 import App from './App';
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={"/"}>
-    <App />
-  </BrowserRouter>,
+  <HttpsRedirect>
+    <BrowserRouter basename={"/"}>
+      <App />
+    </BrowserRouter>
+  </HttpsRedirect>,
   rootElement);
 
 registerServiceWorker();
